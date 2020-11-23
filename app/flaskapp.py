9037952bba23237ccfe3eda3d99037eeb70322d2
LeoDPlouc <leo.lambert02@gmail.com>
2020-11-23 09:51:23 +0100

    Added travis file

--- app/flaskapp.py
+++ app/flaskapp.py
@@ -18,6 +18,7 @@ def visit_counting():
     except RedisError:
         visits = "<i> Could not connect to redis </i>"
     html = "<h1> Number of visits : {}</h1, \n Hostname {}".format(visits, socket.gethostname())
+    return html
 
 if __name__ == "__main__":
     app.run(debug=True, port=80, host='0.0.0.0')
